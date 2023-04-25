5a93ecf733250bb91bc3371b12e04c3ad2c537c6
krakers <92637287+gren2s@users.noreply.github.com>
2023-04-25 20:16:08 +0200

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -1,7 +1,3 @@
-# This code loads the greyscale images of the brain-window
-# for each subject in Patient_CT folder.
-# The code resizes the images and saves them to one folder (train\image).
-
 import os
 from pathlib import Path
 
@@ -24,16 +20,19 @@ hemorrhage_diagnosis_array = hemorrhage_diagnosis_df.values
 # reading images
 AllCTscans = np.zeros([hemorrhage_diagnosis_array.shape[0],  # number of columns (?)
                        new_size[0], new_size[1]], dtype=np.uint8)
+Allsegment = np.zeros([hemorrhage_diagnosis_array.shape[0],
+                       new_size[0], new_size[1]], dtype=np.uint8)
 
 train_path = Path('train')
 image_path = train_path / 'image'
-
+label_path = train_path / 'label'
 if not train_path.exists():
     train_path.mkdir()
     image_path.mkdir()
+    label_path.mkdir()
 
 counterI = 0
-for sNo in tqdm(range(0+49, numSubj+49)):
+for sNo in tqdm(range(0 + 49, numSubj + 49)):
     datasetDirSubj = Path(datasetDir, "{0:0=3d}".format(sNo))
 
     idx = hemorrhage_diagnosis_array[:, 0] == sNo
@@ -46,3 +45,26 @@ for sNo in tqdm(range(0+49, numSubj+49)):
         x = img.resize(new_size)
         AllCTscans[counterI] = x
         x.save(os.path.join(image_path, (str(counterI) + '.png')))
+
+        # Saving the segmentation for a given slice
+        segment_path = Path(datasetDirSubj, 'brain', str(
+            sliceNos[sliceI]) + '_HGE_Seg.jpg')
+        if os.path.exists(str(segment_path)):
+            img = Image.open(segment_path).convert('L')
+            x = img.resize(new_size)
+            # Because of the resize the image has some values that are not 0
+            # or 255, so make them 0 or 255
+            array_from_x = np.array(x)
+            # print(np.unique(array_from_x))
+            array_from_x = np.where(array_from_x > 128, 255, 0)
+            # print(np.unique(array_from_x)) only 0 and 255 values - correct
+            x = Image.fromarray(array_from_x.astype(np.uint8))
+            print(x.getcolors())  # everything looks fine so idk what's wrong
+            x.save(os.path.join(label_path, (str(counterI) + '.png')))
+        else:
+            x = np.zeros([new_size[0], new_size[1]], dtype=np.uint8)
+            x = Image.fromarray(x)
+            x.save(os.path.join(label_path, (str(counterI) + '.png')))
+        Allsegment[counterI] = x
+
+        counterI = counterI + 1
